7ddb95a5b0813f331432034f61933afc7b5a0f2e
Alex Speranza <alex.speranza@studio.unibo.it>
2023-10-17 22:47:03 +0200

    fix: fix configuration of git as the function is not seen by the su shell

--- git.sh
+++ git.sh
@@ -1,9 +1,4 @@
 apt-get install -y git
 
-configure_git() {
-    git config --global user.name "${GIT_USER_NAME}"
-    git config --global user-email "${GIT_USER_EMAIL}"
-}
-
-su -l -c configure_git "${SYSTEM_USER}"
+su -l -c "git config --global user.name '${GIT_USER_NAME}' && git config --global user-email '${GIT_USER_EMAIL}'" "${SYSTEM_USER}"
 
