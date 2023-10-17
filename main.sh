e0b28086ab07384be6e784e3bfe11d04700f05fd
Alex Speranza <alex.speranza@studio.unibo.it>
2023-10-17 22:24:27 +0200

    refactor: remove debug mode

--- main.sh
+++ main.sh
@@ -5,23 +5,14 @@ set -euo pipefail
 # directly prevents half-execution due to a disconnection during
 # download of this script.
 script_body() {
-    # DEBUG mode do not download the files, but it just execute them.
-    # It can be useful if the files have been modified without pushing
-    # the diffs to somewhere.
-    if [ "${DEBUG:-false}" ] ; then
-        download_and_source() {
-            source "$2"
-        }
-    else
-        # Downloads a file and source it.
-        # Params:
-        # $1: the URL of the file
-        # $2: the file name
-        download_and_source() {
-            wget -O "$2" "$1"
-            source "$2"
-        }
-    fi
+    # Downloads a file and source it.
+    # Params:
+    # $1: the URL of the file
+    # $2: the file name
+    download_and_source() {
+        wget -O "$2" "$1"
+        source "$2"
+    }
 
     # Download and allow the user to modify the configuration
     modify_configuration ()
