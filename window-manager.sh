49503f64a95b777ba0a160c69ec44c6f27ad9d9c
Alex Speranza <alex.speranza@studio.unibo.it>
2023-10-18 19:28:30 +0200

    refactor: use the new base url variable

--- window-manager.sh
+++ window-manager.sh
@@ -2,7 +2,7 @@ ${INSTALL_PACKAGE} -y i3 j4-dmenu-desktop lightdm lightdm-gtk-greeter alacritty 
 
 WALLPAPER_PATH="/etc/wallpaper.jpeg"
 
-wget -O "${WALLPAPER_PATH}" "https://raw.githubusercontent.com/asperan/PC-minimal-setup/main/wallpaper/wallpaper-${WALLPAPER_SIZE}.jpeg"
+wget -O "${WALLPAPER_PATH}" "${BASE_URL}/wallpaper-${WALLPAPER_SIZE}.jpeg"
 
 cat <<FEHBG > "/home/${SYSTEM_USER}/.fehbg"
 #!/bin/sh
@@ -12,11 +12,11 @@ FEHBG
 I3_CONFIG_FILE="/home/${SYSTEM_USER}/.config/i3/config"
 
 # Copy i3 config file
-wget -O "${I3_CONFIG_FILE}" "https://raw.githubusercontent.com/asperan/PC-minimal-setup/main/i3/i3.config"
+wget -O "${I3_CONFIG_FILE}" "${BASE_URL}/i3.config"
 
 # Download font and extract it in /usr/local/share/fonts
-wget -O "/tmp/roboto.zip" "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/RobotoMono.zip"
+wget -O "/tmp/roboto.zip" "${BASE_URL}/RobotoMono.zip"
 unzip "/tmp/roboto.zip" -d "/usr/local/share/fonts"
 
-wget -O "/home/${SYSTEM_USER}/.config/alacritty/alacritty.yml" "https://raw.githubusercontent.com/asperan/PC-minimal-setup/main/alacritty/alacritty.yml"
+wget -O "/home/${SYSTEM_USER}/.config/alacritty/alacritty.yml" "${BASE_URL}/alacritty.yml"
 
