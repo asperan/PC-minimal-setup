b871086d6001b9d6281db897cb049e281b2fad4a
Alex Speranza <alex.speranza@studio.unibo.it>
2023-10-18 19:44:09 +0200

    fix: fix creator of fehbg file and creation of config folders

--- window-manager.sh
+++ window-manager.sh
@@ -4,12 +4,14 @@ WALLPAPER_PATH="/etc/wallpaper.jpeg"
 
 wget -O "${WALLPAPER_PATH}" "${BASE_URL}/wallpaper/wallpaper-${WALLPAPER_SIZE}.jpeg"
 
-cat <<FEHBG > "/home/${SYSTEM_USER}/.fehbg"
+su "${SYSTEM_USER}" cat <<FEHBG > "/home/${SYSTEM_USER}/.fehbg"
 #!/bin/sh
 feh --no-fehbg --bg-scale '${WALLPAPER_PATH}'
 FEHBG
 
 I3_CONFIG_FILE="/home/${SYSTEM_USER}/.config/i3/config"
+
+mkdir -p "/home/${SYSTEM_USER}/.config/i3"
 
 # Copy i3 config file
 wget -O "${I3_CONFIG_FILE}" "${BASE_URL}/i3.config"
@@ -17,6 +19,8 @@ wget -O "${I3_CONFIG_FILE}" "${BASE_URL}/i3.config"
 # Download font and extract it in /usr/local/share/fonts
 wget -O "/tmp/roboto.zip" "${BASE_URL}/RobotoMono.zip"
 unzip "/tmp/roboto.zip" -d "/usr/local/share/fonts"
+
+mkdir -p "/home/${SYSTEM_USER}/.config/alacritty"
 
 wget -O "/home/${SYSTEM_USER}/.config/alacritty/alacritty.yml" "${BASE_URL}/alacritty.yml"
 
