d29f35e11f3cc5c4140aceb568769570abef130e
Alex Speranza <alex.speranza@studio.unibo.it>
2023-10-19 09:41:50 +0200

    feat: add variables for font name and neovim repository

--- configuration.sh
+++ configuration.sh
@@ -4,3 +4,7 @@ GIT_USER_NAME="Alex Speranza"
 GIT_USER_EMAIL="alex.speranza@studio.unibo.it"
 
 WALLPAPER_SIZE="1080p" # Values: "1080p", "4k"
+
+FONT="RobotoMono" # from https://github.com/ryanoasis/nerd-fonts/releases/tag/v3.0.2 assets
+
+NVIM_CONFIG_REPOSITORY_URL="https://github.com/asperan/config.nvim.git"
