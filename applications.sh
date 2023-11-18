2e7bb6261cc7cc40bd2c8f54873e26bf7e74e12c
Alex Speranza <alex.speranza@studio.unibo.it>
2023-11-18 10:59:52 +0100

    feat: add installation of more applications and libreoffice language packs

--- applications.sh
+++ applications.sh
@@ -1,5 +1,15 @@
-${INSTALL_PACKAGE} -y firefox thunderbird
-# TODO: Libreoffice
-# TODO: podman
-# TODO: btop
-# TODO: flatpak
+#!/bin/false
+# shellcheck shell=bash
+
+LIBREOFFICE_LANGS="$(tr -s "," " " <<< "${LIBREOFFICE_LANG_PACKS}")"
+
+LIBREOFFICE_PACKAGES="libreoffice-fresh"
+
+for lang in $LIBREOFFICE_LANGS
+do
+    LIBREOFFICE_PACKAGES="${LIBREOFFICE_PACKAGES} libreoffice-fresh-${lang}"
+done
+
+# For libreoffice packages there need to be word splitting
+# shellcheck disable=2086
+${INSTALL_PACKAGE} -y firefox thunderbird ${LIBREOFFICE_PACKAGES} podman btop flatpak
