ac1889cd64d8293ce91db1a9e4f32f15fbd09dbf
Alex Speranza <alex.speranza@studio.unibo.it>
2023-11-18 11:01:15 +0100

    feat: add configuration variable to specify which libreoffice language packs to install

--- configuration.sh
+++ configuration.sh
@@ -8,3 +8,8 @@ WALLPAPER_SIZE="1080p" # Values: "1080p", "4k"
 FONT="RobotoMono" # from https://github.com/ryanoasis/nerd-fonts/releases/tag/v3.0.2 assets
 
 NVIM_CONFIG_REPOSITORY_URL="https://github.com/asperan/config.nvim.git"
+
+# Libreoffice language packs (view https://archlinux.org/packages/?page=1&sort=&repo=Extra&q=libreoffice-fresh)
+# For multiple, separate langs with ','
+# If you do not want to install a language pack, leave the string empty
+LIBREOFFICE_LANG_PACKS=""
